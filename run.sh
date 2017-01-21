e796af575341bda3815baf2b3a9ac57110c4861e
Björn Kjeholt <dev@kjeholt.se>
2017-01-21 22:38:12 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -f
+DOCKER_IMAGE_NAME=bkjeholt/mysql-backup-volume
+DOCKER_CONTAINER_NAME=mysql-backup-volume
+
+DOCKER_IMAGE_BASE_TAG=${1}
+
+ARCHITECTURE=rpi
+
+echo "------------------------------------------------------------------------"
+echo "-- Run image:       $DOCKER_IMAGE_NAME:latest "
+
+DOCKER_IMAGE=${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_BASE_TAG}-${ARCHITECTURE}
+
+echo "------------------------------------------------------------------------"
+echo "-- Remove docker container if it exists"
+echo "-- Container:   $DOCKER_CONTAINER_NAME "
+echo "------------------------------------------------------------------------"
+
+docker rm -f $DOCKER_CONTAINER_NAME
+
+echo "------------------------------------------------------------------------"
+echo "-- Start container "
+echo "-- Based on image: $DOCKER_IMAGE "
+echo "------------------------------------------------------------------------"
+
+docker run -d \
+           --restart=always \
+           --name $DOCKER_CONTAINER_NAME \
+           --link mysql-db-hic:mysql \
+           --vol .:/backup
+           --env DOCKER_CONTAINER_NAME=${DOCKER_CONTAINER_NAME} \
+           $DOCKER_IMAGE
