45aee8046fc45d03df2578cedd98480f30fb9d9e
Björn Kjeholt <dev@kjeholt.se>
2017-01-23 20:49:37 +0100

    Update post-backup.sh

--- Scripts/post-backup.sh
+++ Scripts/post-backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -f
 
-DEFAULT_FILE_NAME=$(date +backup-%g%m%d-%H%M.tar)
+FILE_NAME=$(date +backup-%g%m%d-%H%M.tar)
 DEFAULT_REMOTE_PATH=/backup
 
-FILE_NAME=@{$FTP_BACKUP_FILENAME:-$DEFAULT_FILE_NAME}
+# FILE_NAME=${$FTP_BACKUP_FILENAME:-$DEFAULT_FILE_NAME}
 
-EXTERNAL_PATH=${$FTP_REMOTE_PATH:-$DEFAULT_REMOTE_PATH}
+# EXTERNAL_PATH=${$FTP_REMOTE_PATH:-$DEFAULT_REMOTE_PATH}
 
 echo "------------------------------------------------------------------------"
 echo "-- Upload the backup to volume "
