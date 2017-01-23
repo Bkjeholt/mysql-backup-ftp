b9778c28c2528227f0eb05b56ef314d2909b5043
Björn Kjeholt <dev@kjeholt.se>
2017-01-23 15:19:53 +0100

    Update post-backup.sh

--- Scripts/post-backup.sh
+++ Scripts/post-backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -f
 
 EXTERNAL_FILE_NAME=$(date +backup-%g%m%d-%H%M.tar)
+EXTERNAL_PATH=${FTP_REMOTE_PATH}
 
 echo "------------------------------------------------------------------------"
 echo "-- Upload the backup to volume "
@@ -12,6 +13,7 @@ mv mysql-backup.tar ${EXTERNAL_FILE_NAME}
 ftp ${FTP_HOST_ADDR} <<END_FTP_SCRIPT
 user ${FTP_USER_NAME}
 ${FTP_PASSWORD}
+cd ${EXTERNAL_PATH}
 put ${EXTERNAL_FILE_NAME}
 quit
 END_FTP_SCRIPT
