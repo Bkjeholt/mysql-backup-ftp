8d17913b4507ec97a98ddbac97cd2bbac24020df
Björn Kjeholt <dev@kjeholt.se>
2017-01-24 14:29:29 +0100

    Update init-backup.sh

--- Scripts/init-backup.sh
+++ Scripts/init-backup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -f
 
+
 echo "------------------------------------------------------------------------"
 echo "-- Setup and initiate Volume "
 echo "------------------------------------------------------------------------"
 
+ftp -np ${FTP_HOST_ADDR} <<END_FTP_SCRIPT
+quote USER $FTP_USER_NAME
+quote PASS $FTP_PASSWORD
+mkdir $FTP_BACKUP_BASE_DIR
+cd $FTP_BACKUP_BASE_DIR
+mkdir $DOCKER_CONTAINER_NAME
+cd $DOCKER_CONTAINER_NAME
+pwd
+quit
+END_FTP_SCRIPT
